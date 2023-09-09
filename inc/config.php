4ffaf2a79ac23b01a50f48e51f8974be8e9663c1
ThisaraNavod99 <144094884+ThisaraNavod99@users.noreply.github.com>
2023-09-09 11:57:07 +0530

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="root";
+$dbpass="";
+$host="localhost";
+$db="TheJobsdatabase";
+$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <?php echo date('Y');?> <i class ="fa fa-heartbeat"></i> By Thisara Navod
+           </span>
+          </div>
+        </div>
+</footer>
